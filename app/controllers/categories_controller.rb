8910f311bcfc2e520a6dcddb79f8e5757547c823
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-03 01:29:51 -0400

    added crud actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,31 @@
 class CategoriesController < ApplicationController
+
+    before_action :find_category, only: [:show]
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            redirect_to category_path(@category)
+        else
+            render :new
+        end
+    end
+
+    def show; end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:title, :sport_id)
+    end
+
+    def find_category
+        @category = Category.find(params[:id])
+    end
+
 end
