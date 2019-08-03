da888c3d2f8d08e3cadcc86f5bfb72ee80a84e42
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 21:57:30 -0400

    added signup requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
     root 'welcomes#home' # get '/', to: 'welcomes#home'
 
+    get 'signup', to: 'users#new'
+    post 'signup', to: 'users#create'
+
     resources :teams
     resources :players
     resources :rankings
