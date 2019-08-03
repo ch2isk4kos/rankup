a1ec10e409829af756d8ac5e37ce00ddd2703cf3
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 21:48:29 -0400

    added session helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    # checks for the current user
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    # checks if current user is logged in
+    def logged_in?
+        !!current_user
+    end
+
 end
