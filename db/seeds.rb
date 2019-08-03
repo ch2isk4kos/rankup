b65817cff355d6579d9c3fe24c6ce8ead9719ee3
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-03 00:47:52 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Sport.create!(name: "Basketball", gender: "Men")
+# Sport.create!(name: "Football", gender: "Men")
+# Sport.create!(name: "Baseball", gender: "Men")
+#
+# Category.create!(title: "Top 5 All-Time", sport_id: 1)
+# Category.create!(title: "Top 5 All-Time", sport_id: 2)
+# Category.create!(title: "Top 5 All-Time", sport_id: 3)
+#
+# Player.create(first_name: "Patrick", last_name: "Ewing", position: "Center", number: 33, year: 1994, team_id: 1)
