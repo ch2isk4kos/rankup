0e7c2c99c33fb79306a2c1fbaadf026886f55010
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 20:55:18 -0400

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :teams
-  resources :players
-  resources :rankings
-  resources :categories
-  resources :sports
-  resources :users
+    resources :teams
+    resources :players
+    resources :rankings
+    resources :categories
+    resources :sports
+    resources :users
+    
 end
