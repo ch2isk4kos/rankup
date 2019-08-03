1d86e6739e34b4bab95ac563e937336f347a404a
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 21:14:04 -0400

    routed root page to #home action from  welcomes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+    root 'welcome#home' # get '/', to: 'welcome#home'
+
     resources :teams
     resources :players
     resources :rankings
     resources :categories
     resources :sports
     resources :users
-    
+
 end
