57223d061491a478d096576255293a933d23b67c
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 22:20:42 -0400

    added crud actions to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show; end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
 end
