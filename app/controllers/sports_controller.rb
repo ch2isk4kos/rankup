ce3718db5409db5590422ba951ab92f26bb36e1b
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-03 01:23:10 -0400

    added crud actions to sports controller

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,2 +1,35 @@
 class SportsController < ApplicationController
+
+    before_action :find_sport, only: [:show]
+
+    def index
+        @sports = Sport.all
+    end
+
+    def new
+        @sport = Sport.new
+    end
+
+    def create
+        @sport = Sport.new(sport_params)
+
+        if @sport.save
+            redirect_to sport_path(@sport)
+        else
+            render :new
+        end
+    end
+
+    def show; end
+
+    private
+
+    def sport_params
+        params.require(:sport).permit(:name, :gender)
+    end
+
+    def find_sport
+        @sport = Sport.find(params[:id])
+    end
+
 end
