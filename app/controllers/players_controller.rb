b5ac468ecfdb8ecd98ddf392a43903b5f9b981ec
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-03 01:46:05 -0400

    added crud actions

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,33 @@
 class PlayersController < ApplicationController
+
+    before_action :find_player, only: [:show]
+
+    def new
+        @player = Ranking.new
+    end
+
+    def create
+        @player = Ranking.new(player_params)
+
+        if @player.save
+            redirect_to player_path(@player)
+        else
+            render :new
+        end
+    end
+
+    def show; end
+
+    private
+
+    def player_params
+        params.require(:player).permit(
+            :first_name, :last_name, :position, :number, :year, :ranking_id, :team_id)
+        )
+    end
+
+    def find_player
+        @player = Ranking.find(params[:id])
+    end
+
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,35 @@
 class TeamsController < ApplicationController
+
+    before_action :find_team, only: [:index, :show]
+
+    def index
+        @teams = Team.all
+    end
+
+    def new
+        @team = Ranking.new
+    end
+
+    def create
+        @team = Ranking.new(team_params)
+
+        if @team.save
+            redirect_to team_path(@team)
+        else
+            render :new
+        end
+    end
+
+    def show; end
+
+    private
+
+    def team_params
+        params.require(:team).permit(:city, :name, :year)
+    end
+
+    def find_team
+        @team = Ranking.find(params[:id])
+    end
+
 end
