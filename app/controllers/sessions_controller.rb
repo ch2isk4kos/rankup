1f992f1e774d5c023106ad0090387905decd4070
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-02 21:39:53 -0400

    added crud actions to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    # create new session
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to user
+            # byebug
+        else
+            render :new
+        end
+    end
+
+    # logout
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
 end
